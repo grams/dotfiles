6548b8b9ba95ffda25e5d4bae5a5104932fb90c0
Guillaume Rams <guillaume.rams@ipsen.com>
2015-11-12 10:34:49 +0100

    mysql-workbench actually has a ppa

--- myubuntudesktop.sh
+++ myubuntudesktop.sh
@@ -54,11 +54,11 @@ apt install -y docker-engine libyaml-dev
 pip install -U docker-compose && chmod +x /usr/local/bin/docker-compose
 
 # google chrome
-installWebDeb /usr/bin google-chrome https://dl.google.com/linux/direct google-chrome-stable_current_amd64.deb
+installWebDeb /opt/google/chrome google-chrome https://dl.google.com/linux/direct google-chrome-stable_current_amd64.deb
 
 # tigerVNC
 installWebDeb /usr/bin tigervncserver https://bintray.com/artifact/download/tigervnc/stable/ubuntu-14.04LTS/amd64 tigervncserver_1.5.0-3ubuntu1_amd64.deb
 
 # mysql-workbench
-installWebDeb /usr/bin mysql-workbench http://cdn.mysql.com/Downloads/MySQLGUITools mysql-workbench-community-6.3.4-1ubu1404-amd64.deb
+apt install -y mysql-workbench
 
