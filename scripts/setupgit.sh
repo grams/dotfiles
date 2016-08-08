5f5237718864ccd5de869f2134a8dfd141d7de02
Guillaume Rams <guillaume.rams@ipsen.com>
2016-08-08 13:12:16 +0200

    Added rerere and fix empty variables error

--- scripts/setupgit.sh
+++ scripts/setupgit.sh
@@ -3,11 +3,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-read -e -p "User name? " -i `git config --global user.name` name
-git config --global user.name $name
+read -e -p "User name? " -i "`git config --global user.name`" name
+if [ ! -z "$name" ]; then
+  git config --global user.name $name
+fi
 
-read -e -p "Email? " -i `git config --global user.email` email
-git config --global user.email $email
+read -e -p "Email? " -i "`git config --global user.email`" email
+if [ ! -z "$email" ]; then
+  git config --global user.email $email
+fi
 
 git config --global push.default simple
-
+git config --global rerere.enabled true
+git config --global rerere.autoupdate true
